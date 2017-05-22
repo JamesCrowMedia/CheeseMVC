df9dc7bed32013ee949f5f396bfb748504d53e5d
James Crow <jamescrowvideo@gmail.com>
2017-05-21 20:57:49 -0500

    Finished Class 5 prep

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,21 +12,24 @@ namespace CheeseMVC.Controllers
     public class CheeseController : Controller
     {
 
-        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+        static private List<Cheese> Cheeses = new List<Cheese>();
 
         // GET: /<controller>/
         public IActionResult Index()
         {
-
-            
 
             ViewBag.cheeses = Cheeses;
 
             return View();
         }
 
-        public IActionResult Add()
+        public IActionResult Add(bool error = false)
         {
+            if (error)
+            {
+                ViewBag.error = "You have to name a cheese!";
+            }
+
             return View();
         }
 
@@ -34,7 +38,28 @@ namespace CheeseMVC.Controllers
         public IActionResult NewCheese(string name, string description)
         {
             // Add new cheese to my existing cheeses
-            Cheeses.Add(name, description);
+            if (!String.IsNullOrEmpty(name))
+            {
+                Cheese newCheese = new Cheese(name, description);
+                Cheeses.Add(newCheese);
+
+                return Redirect("/cheese");
+            }
+            else
+            {
+                string error = "Your cheese needs a name";
+
+                return Redirect("/cheese/add?error=True");
+            }
+        }
+        [HttpPost]
+        [Route("/Cheese/Delete")]
+        public IActionResult DeleteCheese(string cheese)
+        {
+            // Add new cheese to my existing cheeses
+
+            int removeIndex = Cheeses.FindIndex(x => x.Name.Equals(cheese));
+            Cheeses.RemoveAt(removeIndex);
 
             return Redirect("/cheese");
         }
